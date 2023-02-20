2647e06038af71ace8fd1dfc4dccc0db5a290e77
Luiz Franzon <luizfranzon05@hotmail.com>
2023-02-20 18:09:09 -0300

    Initial Commit

---
+++ app/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-backgroundBack flex items-center justify-center h-screen">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="text-white text-5xl md:text-7xl p-4">
+      🚧 Em construção 🚧
+    </h1>
+  )
+}
